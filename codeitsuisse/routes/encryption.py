6655101ae58c5387a4fb3639d4622d5ebe0d8c66
dennis03 <dennisleung03@gmail.com>
2021-09-25 04:12:52 +0800

    changes

--- codeitsuisse/routes/encryption.py
+++ codeitsuisse/routes/encryption.py
@@ -8,13 +8,24 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/encryption', methods=['POST'])
-def encryption():
+def evalEncryption():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue * inputValue
+    # inputValue = data.get("input")
+    result = []
+    for inputCase in data:
+        result.append(encrypt(inputCase['n'], inputCase['text']))
     logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    return jsonify(result)
+
+def encrypt(n, text):
+    outText = ''
+    n2 = len(text)//n + 1
+    for i in range(len(text)):
+        n3 = n2*(i%n) + (i-(i%n))//n
+        outText += text[n3]
+    return outText
+
 
 
 
